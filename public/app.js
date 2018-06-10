62f198acdfda6135f81cca17911b3b8e2ac4e47a
Nathaniel Jansen <nathanieljansen@gmail.com>
2018-06-10 17:16:12 -0600

    fix css

--- public/app.js
+++ public/app.js
@@ -12,17 +12,37 @@ $(document).ready(function () {
   function displayResults(data) {
     // console.log(data)
     data.forEach(data => {
-      let row = `
-    <tr>
-      <td>${data.title}</td>
-      <td><img src = "${data.image}"></td>
-      <td>${data.description}</td>
-      <td><a href ="${data.link}">Link</a></td>
-      
-    </tr>
-    `
-      console.log(row);
-      $('tbody').append(row);
+      let header = `
+        <h1>${data.title}</h1>
+      `
+
+      let images = `<div class="row"> 
+      <div class="col-md-4 text-center"> 
+      <img src = "${data.image}"</img>
+      </div>
+      <div class="col-md-6">${data.description}</div>
+      <div class="col-md-2 text-center"><a target="_blank" href ="${data.link}">Link</a></div>
+      </div>
+      `
+      $(".header").append(header)
+      $(".header").append(images)
+
+      let description = ``
+
+      // $(".header").append(description)
+
+
+      //   let row = `
+      // <tr>
+      //   <td>${data.title}</td>
+      //   <td><img src = "${data.image}"></td>
+      //   <td>${data.description}</td>
+      //   <td><a target="_blank" href ="${data.link}">Link</a></td>
+
+      // </tr>
+      // `
+      //   console.log(row);
+      //   $('tbody').append(row);
     });
   }
 })
