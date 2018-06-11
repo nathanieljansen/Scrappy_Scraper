6fa26ed557917606399767e1ccf97bb60b34142f
Nathaniel Jansen <nathanieljansen@gmail.com>
2018-06-11 08:32:45 -0600

    save and note buttons

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  $('.scrapButton').on('click', () => {
+  $('.scrapeButton').on('click', () => {
     console.log("pushed")
     $.getJSON("/scrape", function (data) {
       // Call our function to generate a table body
@@ -16,16 +16,21 @@ $(document).ready(function () {
         <h1>${data.title}</h1>
       `
 
-      let images = `<div class="row"> 
+      let imageDescription = `<div class="row"> 
       <div class="col-md-4 text-center"> 
       <img src = "${data.image}"</img>
       </div>
       <div class="col-md-6">${data.description}</div>
-      <div class="col-md-2 text-center"><a target="_blank" href ="${data.link}">Link</a></div>
+      <div class="col-md-2 text-center link"><a target="_blank" href ="${data.link}">Link</a>
+      <div class="row">
+      <button type="button" class="col-md-12 btn btn-primary saveButton">Save Article</button></div>
+      <div class="row" >
+      <button type="button" class="col-md-12 btn btn-primary noteButton">Make Notes</button></div>
+      </div>
       </div>
       `
       $(".header").append(header)
-      $(".header").append(images)
+      $(".header").append(imageDescription)
 
       let description = ``
 
@@ -44,5 +49,21 @@ $(document).ready(function () {
       //   console.log(row);
       //   $('tbody').append(row);
     });
+
+     $(document).on('click', '.saveButton', function () {
+    
+       $.ajax({
+         method: "POST",
+         url: "./savedarticles",
+         data: this(data)
+       }).then((responseFromBackEnd) => {
+         console.log('Quit it!!', responseFromBackEnd)
+       });
+
+       console.log(JSON.stringify(pairingRecord) + "hi")
+     })
   }
+
+ 
 })
+
